44a12923c4433470271b70488a875f0b9ccf64a0
Khalil Ahmed <k.ahmed2k19@gmail.com>
2021-10-07 11:25:54 +0500

    added git ignore

--- SpywareBackendServer/settings.py
+++ SpywareBackendServer/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -108,21 +107,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'SpywareBackendServer.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cncdb',
-        'USER': 'root',
-        'PASSWORD': 'pligence123',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'NAME': 'YOUR_DB_NAME_HERE',
+        'USER': 'YOUR_DB_USER_HERE',
+        'PASSWORD': 'YOUR_DB_PASSWORD_HERE',
+        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -141,7 +138,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
@@ -166,7 +162,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -185,3 +180,7 @@ except:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+try:
+    from local_settings import *
+except Exception as exc:
+    print(exc)
